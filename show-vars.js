e5badfbafb89aa3b459ac2a247e4076279a81111
Felipe Orlando <fobsouza@gmail.com>
2018-03-29 11:24:52 -0300

    Show vars

---
+++ show-vars.js
@@ -0,0 +1,7 @@
+console.log('CI_HOST: ', process.env.CI_HOST);
+console.log('LIGHTHOUSE_API_KEY: ', process.env.LIGHTHOUSE_API_KEY);
+console.log('API_KEY: ', process.env.API_KEY);
+console.log('TRAVIS_PULL_REQUEST: ', process.env.TRAVIS_PULL_REQUEST);
+console.log('TRAVIS_PULL_REQUEST_SHA: ', process.env.TRAVIS_PULL_REQUEST_SHA);
+console.log('TRAVIS_PULL_REQUEST_SLUG: ', process.env.TRAVIS_PULL_REQUEST_SLUG);
+console.log('TRAVIS_EVENT_TYPE: ', process.env.TRAVIS_EVENT_TYPE);
